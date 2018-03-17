f654f407d2a92393a0b60102eff5a9a4fe0be1ce
somalieca <bigman_cr@abv.bg>
2018-03-17 17:04:10 +0200

    Added user login

--- app.js
+++ app.js
@@ -4,13 +4,30 @@ var mysql = require('mysql');
 
 var con = mysql.createConnection({
   host: "localhost",
-  user: "root",
-  password: "root"
+  user: "kristiqn",
+  password: "hacktues",
+  database : "InternShipper"
 });
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
+});
+
+app.get('/login/', function (req, res) {
+	console.log(req.query.email);
+	if (!req.query.email) {
+		res.json({Error : 'Please enter a valid email'});
+	} else if (!req.query.email) {
+		res.json({Error : 'Please enter a valid password'});
+	}
+
+	con.query('SELECT * FROM Person WHERE email = "' + req.query.email + '";' , function (err, result, fields) {
+		console.log(result);
+		if (result.passwordHash === req.query.password) {
+			res.json(result);
+		}
+	});
 });
 
 app.listen(3000);
